e0d1c16b692e32eb68270b8b09868eb23d75512a
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-05-03 19:41:59 -0500

    Added delete button. Button is not functional yet.

--- models/burger.js
+++ models/burger.js
@@ -17,6 +17,12 @@ var burger = {
         orm.updateOne("burgers", vals, condition, function(res) {
             cb(res);
         });
+    },
+
+    deleteOne: function(condition, cb) {
+        orm.deleteOne("burgers", condition, function(res) {
+            cb(res);
+        });
     }
 }
 
